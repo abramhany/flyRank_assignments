e7cfd26ee82a494d4237035df9a89b91ff216f86
abram <abramhany9@gmail.com>
2026-08-20 18:52:37 +0300

    added post method

--- week2/main.py
+++ week2/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+#from enum import Enum
 
 app = FastAPI()
 
@@ -8,8 +9,19 @@ def status():
 items = {
     1:"house",2:'mouse'
 }
+
+@app.get("/item/all")
+async def get_all():
+    return items
+
 @app.get('/item/{id}')
-def add_item(id:int):
+async def get_item(id:int):
     if id > len(items):
         return "not there"
     return items[id]
+
+@app.post('/item/{name}')
+async def create_item(item_name:str):
+    id = len(items) +1
+    items[id] = item_name
+    return {id:items[id]}
