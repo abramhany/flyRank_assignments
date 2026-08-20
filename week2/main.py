0b2162dd43c7b7caec60a88ef213990d52049e24
abram <abramhany9@gmail.com>
2026-08-20 19:02:22 +0300

    added infro and status function

--- week2/main.py
+++ week2/main.py
@@ -4,11 +4,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def status():
-    return {'Status':"Online"}
+async def info():
+    return {'name':"Task API",'version':"1.0","endpoints":['/item']}
 items = {
     1:"house",2:'mouse'
 }
+
+@app.get('/health')
+async def status():
+    return {'status':"ok"}
 
 @app.get("/item/all")
 async def get_all():
