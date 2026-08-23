d54af70ec87072006658df17e4070b4a236d64f8
abram <abramhany9@gmail.com>
2026-08-23 08:45:33 +0300

    Stage 4: full CRUD

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
-from fastapi import FastAPI , status
+from fastapi import FastAPI , HTTPException 
 from pydantic import BaseModel , Field
 
 
 class Item(BaseModel):
     title:str = Field(min_length=1, strip_whitespace=True)
 
+class Update(BaseModel):
+    title:str | None = None
+    done:bool | None = None
+    
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/",status_code=201)
 async def info():
     return {'name':"Task API",'version':"1.0","endpoints":['/tasks']}
 items = [
@@ -20,30 +24,58 @@ items = [
 async def status():
     return {'status':"ok"}
 
-@app.get("/task/all")
+@app.get("/tasks",status_code=200)
 async def get_all():
     return items[:]
 
 @app.get('/tasks/{id}',status_code=200)
-async def get_item(id:int):
+async def get_task(id:int):
     if id > len(items):
-        return 400,{"error": f"Task {id} not found"}
+        raise HTTPException(status_code=400,detail=f"Task {id} not found")
     return 200 ,items[id]
 
-@app.post('/tasks/')
-async def create_item(item:Item):
+@app.post('/tasks',status_code=200)
+async def create_task(item:Item):
     if item.title.strip() == '':
-        return 400,{'Error':"Bad Request"}
+        raise HTTPException(status_code=400,detail="Bad request")
+        
 
-    
     id = len(items)
-
     dic = {
         'id':id,
         "title":item.title,
         "done":False
     }
-
     items.append(dic)
     print(id)
     return 201,items[id]
+
+@app.put('/tasks/{id}')
+async def update_task(id:int,update:Update):
+    if id > len(items):
+        raise HTTPException(status_code=404,detail="Couldn't find ID")
+    
+    if update.done is None and update.title is None:
+        raise HTTPException(status_code=400,detail="Couldn't update the task")
+
+    dic = items[id]
+
+    if update.done is not None:
+        dic['done'] = update.done
+
+    if update.title is not None:
+        dic['title'] = update.title
+
+    return items[id]
+
+@app.delete('/tasks/{id}',status_code=204)
+async def update_task(id:int):
+    if id<0 or id > len(items):
+        raise HTTPException(status_code=404,detail="Couldn't find ID")
+    
+    del items[id]
+
+    return "No Content"
+        
+
+    
